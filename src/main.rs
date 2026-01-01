76bd8a9f7c2b80af296fb6e593c6cc4297a15d71
Adamzz <cybergi10@googlemail.com>
2026-01-01 19:44:28 +0200

    added some more features still not fully functional

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::{io::{self, Write}, time::Duration};
 use std::thread::sleep;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 
 
@@ -28,6 +30,7 @@ impl Pos{
 
         Some((new_x,new_y))
     }
+
     fn rotate_y(&self, angle: f32) -> Pos {
         let cos = angle.cos();
         let sin = angle.sin();
@@ -64,62 +67,180 @@ impl Pos{
 
 
 }
+
 #[derive(Clone)]
-struct Object{
-    vertices: Vec<Pos>,
-    lines: Vec<(usize,usize)>
-}
-impl Object{
-    fn new() -> Self{
-        let vertices : Vec<Pos> = Vec::new();
-        let lines: Vec<(usize,usize)> = Vec::new();
-        Self {
-            vertices,
-            lines
-        }
-    }
-    fn shift(&mut self,vector: Pos){
-        for item in &mut self.vertices{
-            item.x += vector.x;
-            item.y += vector.y;
-            item.z += vector.z;
-        }
-    }
+struct Triangle {
+    v0: Pos,
+    v1: Pos,
+    v2: Pos,
 }
 
+impl Triangle {
+    fn new(v0: Pos, v1: Pos, v2: Pos) -> Self {
+        Self { v0, v1, v2 }
+    }
+}
+#[derive(Clone)]
+struct Mesh {
+    triangles: Vec<Triangle>,
+}
+
+impl Mesh {
+    fn new() -> Self {
+        Self { triangles: Vec::new() }
+    }
+    fn from_obj() -> Self{
+        // TODO 
+        Self {triangles: Vec::new()}
+    }
+    }
+
+    fn shift(&mut self, offset: Pos) {
+        for tri in &mut self.triangles {
+            tri.v0.x += offset.x; tri.v0.y += offset.y; tri.v0.z += offset.z;
+            tri.v1.x += offset.x; tri.v1.y += offset.y; tri.v1.z += offset.z;
+            tri.v2.x += offset.x; tri.v2.y += offset.y; tri.v2.z += offset.z;
+        }
+    }
+
+    fn rotate_x_about_axis(&mut self, angle: f32,axis: Option<Pos>) {
+        let center = if axis.is_none(){
+            self.get_center()
+        }else{
+            axis.unwrap()
+        };
+
+        for tri in &mut self.triangles {
+            for v in [&mut tri.v0, &mut tri.v1, &mut tri.v2] {
+                let shifted = Pos::new(v.x - center.x, v.y - center.y, v.z - center.z).rotate_x(angle);
+                v.x = shifted.x + center.x;
+                v.y = shifted.y + center.y;
+                v.z = shifted.z + center.z;
+            }
+        }
+    }
+    fn rotate_y_about_axis(&mut self, angle: f32,axis: Option<Pos>) {
+        let center = if axis.is_none(){
+            self.get_center()
+        }else{
+            axis.unwrap()
+    };
+
+        for tri in &mut self.triangles {
+            for v in [&mut tri.v0, &mut tri.v1, &mut tri.v2] {
+                let shifted = Pos::new(v.x - center.x, v.y - center.y, v.z - center.z).rotate_y(angle);
+                v.x = shifted.x + center.x;
+                v.y = shifted.y + center.y;
+                v.z = shifted.z + center.z;
+            }
+        }
+    }
+    fn rotate_z_about_axis(&mut self, angle: f32,axis: Option<Pos>) {
+        let center = if axis.is_none(){
+            self.get_center()
+        }else{
+            axis.unwrap()
+    };
+
+    for tri in &mut self.triangles {
+        for v in [&mut tri.v0, &mut tri.v1, &mut tri.v2] {
+            let shifted = Pos::new(v.x - center.x, v.y - center.y, v.z - center.z).rotate_z(angle);
+            v.x = shifted.x + center.x;
+            v.y = shifted.y + center.y;
+            v.z = shifted.z + center.z;
+        }
+    }
+    }
+    
+    fn get_center(&self) -> Pos {
+        let mut min = self.triangles[0].v0.clone();
+        let mut max = self.triangles[0].v0.clone();
+        for tri in &self.triangles {
+            for v in [&tri.v0, &tri.v1, &tri.v2] {
+                min.x = min.x.min(v.x); min.y = min.y.min(v.y); min.z = min.z.min(v.z);
+                max.x = max.x.max(v.x); max.y = max.y.max(v.y); max.z = max.z.max(v.z);
+            }
+        }
+        Pos {
+            x: (min.x + max.x) / 2.0,
+            y: (min.y + max.y) / 2.0,
+            z: (min.z + max.z) / 2.0,
+        }
+    }
+}
+fn cube_mesh(size: f32, depth: f32) -> Mesh {
+    let mut mesh = Mesh::new();
+
+    let p = |x,y,z| Pos::new(x,y,z);
+
+    // Cube vertices
+    let v = [
+        p(-size, -size, depth),      // 0
+        p( size, -size, depth),      // 1
+        p(-size,  size, depth),      // 2
+        p( size,  size, depth),      // 3
+        p(-size, -size, depth+size*3.0), // 4
+        p( size, -size, depth+size*3.0), // 5
+        p(-size,  size, depth+size*3.0), // 6
+        p( size,  size, depth+size*3.0), // 7
+    ];
+
+    // Front face
+    mesh.triangles.push(Triangle::new(v[0], v[1], v[2]));
+    mesh.triangles.push(Triangle::new(v[1], v[3], v[2]));
+
+    // Back face
+    mesh.triangles.push(Triangle::new(v[4], v[6], v[5]));
+    mesh.triangles.push(Triangle::new(v[5], v[6], v[7]));
+
+    // Top face
+    mesh.triangles.push(Triangle::new(v[2], v[3], v[6]));
+    mesh.triangles.push(Triangle::new(v[3], v[7], v[6]));
+
+    // Bottom face
+    mesh.triangles.push(Triangle::new(v[0], v[4], v[1]));
+    mesh.triangles.push(Triangle::new(v[1], v[4], v[5]));
+
+    // Left face
+    mesh.triangles.push(Triangle::new(v[0], v[2], v[4]));
+    mesh.triangles.push(Triangle::new(v[2], v[6], v[4]));
+
+    // Right face
+    mesh.triangles.push(Triangle::new(v[1], v[5], v[3]));
+    mesh.triangles.push(Triangle::new(v[3], v[5], v[7]));
+
+    mesh
+}
 
 
 fn main() {
 
-    const SLEEP_TIME: u64 = 1000/60;
-
-    //let mut points: Vec<Pos> = Vec::new();
-
-//    points.push(Pos::new(0.1, 0.1, 0.2));
-  //  points.push(Pos::new(0.9, 0.9, 0.2));
+    const SLEEP_TIME: u64 = 700/60;
 
     // let's define a cube 
 
-    let mut cube: Object = Object::new();
-    let size = 1.0;
-    let depth = 5.0;
+    // let mut cube: Object = Object::new();
+    // let size = 1.0;
+    // let depth = 5.0;
 
 
-    cube.vertices.push(Pos::new(-size, -size, depth));           // 0
-    cube.vertices.push(Pos::new( size, -size, depth));           // 1
-    cube.vertices.push(Pos::new(-size,  size, depth));           // 2
-    cube.vertices.push(Pos::new( size,  size, depth));           // 3
-    cube.vertices.push(Pos::new(-size, -size, depth + size * 3.0));    // 4
-    cube.vertices.push(Pos::new( size, -size, depth + size * 3.0));    // 5
-    cube.vertices.push(Pos::new(-size,  size, depth + size * 3.0));    // 6
-    cube.vertices.push(Pos::new( size,  size, depth + size * 3.0));    // 7
+    // cube.vertices.push(Pos::new(-size, -size, depth));           // 0
+    // cube.vertices.push(Pos::new( size, -size, depth));           // 1
+    // cube.vertices.push(Pos::new(-size,  size, depth));           // 2
+    // cube.vertices.push(Pos::new( size,  size, depth));           // 3
+    // cube.vertices.push(Pos::new(-size, -size, depth + size * 5.0));    // 4
+    // cube.vertices.push(Pos::new( size, -size, depth + size * 5.0));    // 5
+    // cube.vertices.push(Pos::new(-size,  size, depth + size * 5.0));    // 6
+    // cube.vertices.push(Pos::new( size,  size, depth + size * 5.0));    // 7
 
-    // All 12 edges...
-    cube.lines.extend_from_slice(&[
-        (0,1), (1,3), (3,2), (2,0),
-        (4,5), (5,7), (7,6), (6,4),
-        (0,4), (1,5), (2,6), (3,7),
-    ]);
+    // // All 12 edges...
+    // cube.lines.extend_from_slice(&[
+    //     (0,1), (1,3), (3,2), (2,0),
+    //     (4,5), (5,7), (7,6), (6,4),
+    //     (0,4), (1,5), (2,6), (3,7),
+    // ]);
+
+    let mut cube_mesh = cube_mesh(1.0, 1.0);
 
 
     let mut angle = 0.0;
@@ -130,17 +251,20 @@ fn main() {
         clear_screen();
         let term_size = get_terminal_size();
         
-        let mut cube = cube.clone();
-        for vertex in &mut cube.vertices {
-            *vertex = vertex.rotate_y(angle);
-        }
-        
+        // let mut cube = cube.clone();
+        // cube.rotate_x_about_axis(angle, None);
+        // cube.rotate_y_about_axis(angle, None);
+        // cube.rotate_z_about_axis(angle, None);
         // draw dat shii
-        for item in &cube.lines {
-            let _ = draw_line(cube.vertices[item.0], cube.vertices[item.1], '.', term_size);
-        }
+        // for item in &cube.lines {
+        //     let _ = draw_line(cube.vertices[item.0], cube.vertices[item.1], '*', term_size);
+        // }
+        let mut mesh = cube_mesh.clone();
+        mesh.rotate_x_about_axis(angle,None);
+        mesh.rotate_y_about_axis(angle,None);
+        draw_mesh(&mesh, term_size);
         
-        angle += 0.05;  // Spin!
+        angle += 0.06;  // Spin!
     }
 }
 
@@ -150,6 +274,15 @@ fn clear_screen(){
     print!("\x1B[2J");
     print!("\x1B[?25l"); // hide cursor
 }
+
+fn draw_mesh(mesh: &Mesh, screen_size: (u16, u16)) {
+    for tri in &mesh.triangles {
+        draw_line(tri.v0, tri.v1, '*', screen_size);
+        draw_line(tri.v1, tri.v2, '*', screen_size);
+        draw_line(tri.v2, tri.v0, '*', screen_size);
+    }
+}
+
 
 
 
